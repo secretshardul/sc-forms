a9270725d29059556314def4be621833f2640284
shardul <shardul.aeer@somaiya.edu>
2021-02-17 16:59:57 +0530

    feat: Home screen

--- src/home/HomeScreen.js
+++ src/home/HomeScreen.js
@@ -1,10 +1,99 @@
-import SignInSide from './SignInSide'
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Icon from '@material-ui/core/Icon';
+import logo from './balance.svg';
 
-function HomeScreen () {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: 'url(https://i1.wp.com/mogollonrimnews.com/wp-content/uploads/2017/02/SupremeCt.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(7),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    imageIcon: {
+        // height: '100%',
+        maxHeight: 70
+    },
+    iconRoot: {
+        // textAlign: 'center'
+        maxHeight: 40
+
+    },
+}))
+
+export default function HomeScreen () {
+    const classes = useStyles()
+
     return (
-        // <div>Home</div>
-        <SignInSide />
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <img className={classes.imageIcon} src={logo} />
+                    <Typography variant="h4">
+                        Court.ly
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Court forms made simple
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
     )
 }
-
-export default HomeScreen
